bcf7003008552663f3688802fb4cecd53e9f15b6
SHAURYA SINGH <shaurya7singh@gmail.com>
2021-03-13 21:48:24 +0530

    done

---
+++ precache-manifest.08a3a01c141e61ecc01519c8a0840cc4.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5c11db41228b078cbe3ecd5222ae496",
+    "url": "/SHAURYA369/shauryarepo/index.html"
+  },
+  {
+    "revision": "5269048f684d063a61df",
+    "url": "/SHAURYA369/shauryarepo/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "b4fd63d27d5f7dd583c0",
+    "url": "/SHAURYA369/shauryarepo/static/css/4.ea3dbbf2.chunk.css"
+  },
+  {
+    "revision": "a90eeb30be45b89600c8",
+    "url": "/SHAURYA369/shauryarepo/static/css/main.85a76908.chunk.css"
+  },
+  {
+    "revision": "41de9dafc789cd69e68c",
+    "url": "/SHAURYA369/shauryarepo/static/js/2.fc07d5bc.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/SHAURYA369/shauryarepo/static/js/2.fc07d5bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5269048f684d063a61df",
+    "url": "/SHAURYA369/shauryarepo/static/js/3.922f070e.chunk.js"
+  },
+  {
+    "revision": "b4fd63d27d5f7dd583c0",
+    "url": "/SHAURYA369/shauryarepo/static/js/4.b8e4f4a8.chunk.js"
+  },
+  {
+    "revision": "a90eeb30be45b89600c8",
+    "url": "/SHAURYA369/shauryarepo/static/js/main.adfc9d67.chunk.js"
+  },
+  {
+    "revision": "f0a03bc315530a93302f",
+    "url": "/SHAURYA369/shauryarepo/static/js/runtime-main.ad110b15.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/SHAURYA369/shauryarepo/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/SHAURYA369/shauryarepo/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c459e36694e859e8348b1425d1ecafa4",
+    "url": "/SHAURYA369/shauryarepo/static/media/ares.c459e366.jpg"
+  },
+  {
+    "revision": "99c12ae9c5ad32a5729e7bf8b6514238",
+    "url": "/SHAURYA369/shauryarepo/static/media/chinmaya.99c12ae9.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/SHAURYA369/shauryarepo/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/SHAURYA369/shauryarepo/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "967c3675b3f2da910b36bd23cc05e8da",
+    "url": "/SHAURYA369/shauryarepo/static/media/lang.967c3675.jpg"
+  },
+  {
+    "revision": "ddaa653180568d16af901f637dcbc38f",
+    "url": "/SHAURYA369/shauryarepo/static/media/lol.ddaa6531.jpg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/SHAURYA369/shauryarepo/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "15e15f53c3d6da7b5c7349960d312caf",
+    "url": "/SHAURYA369/shauryarepo/static/media/mars.15e15f53.png"
+  },
+  {
+    "revision": "8dee06b66ff2ea396ebbc0977bcb2a4b",
+    "url": "/SHAURYA369/shauryarepo/static/media/nescii.8dee06b6.png"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/SHAURYA369/shauryarepo/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "7f81034fc16a3c345308ef92db54a8a6",
+    "url": "/SHAURYA369/shauryarepo/static/media/nsut.7f81034f.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/SHAURYA369/shauryarepo/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/SHAURYA369/shauryarepo/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/SHAURYA369/shauryarepo/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "746dfd92a8793d71564a0b7c9ee9e033",
+    "url": "/SHAURYA369/shauryarepo/static/media/techx.746dfd92.png"
+  }
+]);
